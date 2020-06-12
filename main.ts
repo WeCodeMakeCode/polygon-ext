45163a13a26bb4f4f1ee8f34c5732102f62da515
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-06-11 22:08:24 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ namespace polygon {
     export function createSpinner(polygon:Polygon, speed:number): Spinner {
         let spinner = new Spinner(polygon,speed);
         game.onUpdate(function () {
-            spinner.polygon.angle = (spinner.polygon.angle + 15) % 360
+            spinner.polygon.angle = (spinner.polygon.angle + 360/speed) % 360
         })
         return spinner;
     }
