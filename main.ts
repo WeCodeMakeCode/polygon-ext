4e5a8f5de805de86fa78c03b32e88ab63a4be7d0
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-06-11 22:03:01 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 //%  weight=100 color color=#008080  blockGap=8
+//% groups='["Polygon", "Spinner"]'
 namespace polygon {
     //% block="create polygon with %n_sides sides radius %radius || color %color angle %angle"
     //% blockSetVariable=myPolygon
@@ -8,12 +9,14 @@ namespace polygon {
     //% radius.min=10 adius.max=50 radius.defl=30
     //% color.min=1 color.max=15 color.defl=2  
     //% angle.min=0 angle.max=360 angle.defl=0
+    //% group="Polygon"
     export function createPolygon(n_sides: number, radius: number, color: number = 2, angle: number = 0): Polygon {
         return new Polygon(n_sides, radius, color, angle);
     }
     //% block="create spinner from %polygon=variables_get(myPolygon) ith speed %speed"
     //% blockSetVariable=mySpinner
     //% speed.min=0 speed.max=10 speed.defl=5
+    //% group="Spinner"
     export function createSpinner(polygon:Polygon, speed:number): Spinner {
         let spinner = new Spinner(polygon,speed);
         game.onUpdate(function () {
@@ -28,14 +31,16 @@ class Spinner{
     private _speed:number = 0;
     //% blockSetVariable="mySpinner"
     //% blockCombine block="polygon"
+    //% group="Spinner"
     get polygon(): Polygon {
         return this._polygon;
-    }
+    }//% group="Spinner"
     //% blockSetVariable="mySpinner"
     //% blockCombine block="speed"
     get speed(): number {
         return this._speed;
     }
+    //% group="Spinner"
     //% blockSetVariable="mySpinner"
     //% blockCombine block="speed"
     set speed(value: number) {
@@ -55,23 +60,26 @@ class Polygon {
     private _color: number = 2;
     private _angle: number = 0;
     private _types: string[] = ["", "", "", "triangle 3", "square 4", "pentagon 5", "hexagon 6", "heptagon 7", "Octagon 8", "Nonagon 9", "Decagon 10", "Hendecagon 11", "Dodecagon 12", "Triskaidecagon 13", "Tetrakaidecagon 14", "Pentadecagon 15", "Hexakaidecagon 16", "Heptadecagon 17", "Octakaidecagon 18", "Enneadecagon 19","Icosagon 20"];
-
+    //% group="Polygon"
     //% lockSetVariable="myPolygon"
     //% blockCombine block="sides"
     get sides(): number {
         return this._n_sides;
     }
+    //% group="Polygon"
     //% blockSetVariable="myPolygon"
     //% blockCombine block="sides"
     set sides(value: number) {
         this._n_sides = Math.min(Math.max(value, 3), 30);
         this.draw_polygon();
     }
+    //% group="Polygon"
     //% blockSetVariable="myPolygon"
     //% blockCombine block="radius"
     get radius(): number {
         return this._radius;
     }
+    //% group="Polygon"
     //% blockSetVariable="myPolygon"
     //% blockCombine block="radius"
     set radius(value: number) {
@@ -81,33 +89,39 @@ class Polygon {
         this._polygon = sprites.create(this._img, SpriteKind.Player)
         this.draw_polygon();
     }
+    //% group="Polygon"
     //%  blockSetVariable="myPolygon"
     //% blockCombine block="color"
     get color(): number {
         return this._color;
     }
+    //% group="Polygon"
     //% blockSetVariable="myPolygon"
     //% blockCombine block="color"
     set color(value: number) {
         this._color = Math.min(Math.max(value, 1), 15);
         this.draw_polygon();
     }
+    //% group="Polygon"
     //% blockSetVariable="myPolygon"
     //% blockCombine block="starting angle degrees"
     get angle(): number {
         return this._angle;
     }
+    //% group="Polygon"
     //% blockSetVariable="myPolygon"
     //% blockCombine block="starting angle degrees"
     set angle(value: number) {
         this._angle =  (value % 360 ) + 1;   //Math.min(Math.max(value, 0), 360);
         this.draw_polygon();
     }
+    //% group="Polygon"
     //% blockSetVariable="myPolygon"
     //% blockCombine block="sprite"
     get sprite(): Sprite {
         return this._polygon;
     }
+    //% group="Polygon"
     //% blockSetVariable="myPolygon"
     //% blockCombine block="type"
     get type(): string {
